86ddd7e8efe4eac0e0e441c50d25b94ee2a9d3dd
Szybki <Szybki@DESKTOP-TUIKJEF>
2024-05-03 03:32:24 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+
+namespace HelloWorld
+{
+    //PascalCase
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //camelCase
+            string firstAndLastName = "Michal";
+
+            //UPPER_CASE
+            const double PI_NUMBER = 3.14;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam sie Michal Wolter");
+            if (PI_NUMBER > 2) 
+            {
+                WritePiNumber(PI_NUMBER);
+            }
+        }
+        private static string WritePiNumber(double pi) 
+        {
+            return ("3.14");
+        }
+    }
+}
+
